e0ca455c6bc77d1a35832e505222f7e464c10e7b
Pablo Ruiz Múzquiz <diacritica@elenya.net>
2014-07-14 17:07:35 +0200

    Adding first attempt at passing the test. test slightly modified to be more realistic. Test is failing.

--- graffinity/__init__.py
+++ graffinity/__init__.py
@@ -1,6 +1,91 @@
+
 class Graffinity(object):
     def __init__(self, data, funcs):
-        pass
+
+        self.data = data
+
+        self.f = {}
+        self.functionresults = {}
+        self.matrix = {}
+
+        funcs.pop("affinity") #to workaround test's temporary inconsistency
+
+        for func in funcs.keys():
+          funcdata = [(d[0],d[1][func]) for d in data.items()]
+          self.f[func] = {'data':funcdata}
+          self.f[func]['funcdef'] = funcs[func]
+
+        nodenames = data.keys()
+
+        for datum in nodenames:
+          self.matrix[datum] = {}
+          for otherdatum in nodenames:
+            self.matrix[datum][otherdatum] = 100
+
+        print("initial matrix", self.matrix)
 
     def calculate(self):
-        pass
+
+        for isolatedfunc in self.f.items():
+          self.calculateisolatedfunction(isolatedfunc)
+
+        print("******************************************")
+        print("final results per function",self.functionresults)
+
+        #with individual function results do stuff and update self.matrix
+
+
+        fr = self.functionresults
+
+        for n in self.matrix:
+          for m in self.matrix:
+            try:
+              self.matrix[n][m] = self.checkreverse(fr['gender'],n,m) + self.checkreverse(fr['age'],n,m) + self.checkreverse(fr['languages'],n,m)
+            except:
+              pass
+
+        print("final matrix",self.matrix)
+        for functionresult in self.functionresults.items():
+          print(functionresult)
+
+        return self.matrix
+
+    def calculategroup(self, group):
+      pass
+
+    def calculateisolatedfunction(self, isolatedfunc):
+
+        print("Processing function: ",isolatedfunc[0])
+        result = {}
+        funcdictresult = {}
+
+        #will make tuples of consecutive items in a list (nodepairs)
+        f = lambda data: [data[i:i+2] for i in range(len(data) - 1)]
+
+        nodepairs = f(isolatedfunc[1]['data'])
+
+        for nodepair in nodepairs:
+          names, values = list(zip(*nodepair))
+
+          flatvalues = [item for sublist in values for item in sublist]
+
+          for n in names:
+            result = isolatedfunc[1]['funcdef'](flatvalues)
+            funcdictresult[names[0]] = {names[1]:result}
+
+
+        print("******************************************")
+        print(isolatedfunc[0],funcdictresult)
+        self.functionresults[isolatedfunc[0]] = funcdictresult
+
+        return
+
+    def checkreverse(self, function, n, m):
+      try:
+        print("as is! ",function[n][m])
+        return function[n][m]
+      except:
+        print("reverse! ",function[m][n])
+        return function[m][n]
+
+
--- graffinity/tests/test_foo.py
+++ graffinity/tests/test_foo.py
@@ -1,21 +1,35 @@
 from unittest import TestCase
+import unittest
 
 import graffinity
+import statistics
+
 
 class TestFoo(TestCase):
 
     def test_basic_calculation(self):
         data = {
             "n1": {
-                "gender": 0,
-                "age": 36,
+                "gender": [0],
+                "age": [36],
                 "languages": [2,5,6],
             },
             "n2": {
-                "gender": 1,
-                "age": 33,
+                "gender": [1],
+                "age": [33],
                 "languages": [2,6],
+            },
+            "n3": {
+                "gender": [1],
+                "age": [25],
+                "languages": [1,6,9,10],
+            },
+            "n4": {
+                "gender": [1],
+                "age": [28],
+                "languages": [1],
             }
+
         }
 
         gender_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
@@ -31,7 +45,11 @@ class TestFoo(TestCase):
         }
         g = graffinity.Graffinity(data, funcs)
         results = g.calculate()
-        self.assertEquals(results["n1"]["n1"], 100)
-        self.assertEquals(results["n1"]["n2"], 56)
-        self.assertEquals(results["n2"]["n1"], 56)
-        self.assertEquals(results["n2"]["n2"], 100)
+        self.assertEqual(results["n1"]["n1"], 100)
+        self.assertEqual(results["n1"]["n2"], 6.106775831422421)
+        self.assertEqual(results["n2"]["n1"], 6.106775831422421)
+        self.assertEqual(results["n2"]["n2"], 100)
+
+
+if __name__=="__main__":
+  unittest.main()
