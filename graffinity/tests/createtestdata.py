d5d0e6c40014187282884efadbd2c34769370135
Pablo Ruiz Múzquiz <diacritica@elenya.net>
2014-07-15 15:41:51 +0200

    Added a full test for time perfomance

--- graffinity/tests/createtestdata.py
+++ graffinity/tests/createtestdata.py
@@ -1,16 +1,61 @@
-from random import choice, sample
+from random import choice, sample, randint
+
+n = 500
 
 def datagenerator(n=1000):
     datadict = {}
     for i in range(n):
-        datadict["n"+str(i)] = {"gender":[choice(genderchoices)],"age":[choice(agechoices)],"languages":sample(languageslist,3)}
+        datadict["n"+str(i)] =   {
+	"gender": [choice(genderchoices)],
+	"age": [choice(agechoices)],
+	"languages": sample(languageslist,3),
+	"friends": sample(friendslist,randint(1,5)),
+	"knowngames": sample(knowngameslist,randint(1,20)),
+	"toplaywishlist": sample(toplaywishlistlist,randint(1,20)),
+	"favouritegames": sample(favouritegameslist,randint(1,20)),
+	"preferences": sample(preferenceslist,randint(1,3)),
+	"vetoes": sample(vetoeslist,randint(1,3)),
+	"gametype": sample(gametypelist,randint(1,5)),
+	"skill01": [choice(skill01choices)],
+	"skill02": [choice(skill02choices)],
+	"skill03": [choice(skill03choices)],
+	"skill04": [choice(skill04choices)],
+	"skill05": [choice(skill05choices)],
+	"skill06": [choice(skill06choices)],
+	"skill07": [choice(skill07choices)],
+	"skill08": [choice(skill08choices)],
+	"skill09": [choice(skill09choices)],
+	"skill10": [choice(skill10choices)],
+	"guilds": sample(guildslist,randint(1,5)),
+	}
+
     return datadict
+
+
 
 genderchoices = [1,2]
 agechoices = [i for i in range(18,70)]
 languageslist = [i for i in range(1,20)]
+friendslist = [i for i in range(1,n)]
+knowngameslist =  [i for i in range(1,5000)]
+toplaywishlistlist = knowngameslist
+favouritegameslist = knowngameslist
+preferenceslist = [i for i in range(1,5)]
+vetoeslist = [i for i in range(1,5)]
+gametypelist = [i for i in range(1,64)]
+skill01choices = [i for i in range(1,100)]
+skill02choices = skill01choices
+skill03choices = skill01choices
+skill04choices = skill01choices
+skill05choices = skill01choices
+skill06choices = skill01choices
+skill07choices = skill01choices
+skill08choices = skill01choices
+skill09choices = skill01choices
+skill10choices = skill01choices
+guildslist = [i for i in range(1,200)]
 
-h=datagenerator(1000)
+h=datagenerator(n)
 fi = open("atest.py",'w')
 fi.write(str(h))
 fi.close()
--- graffinity/tests/time_performance.py
+++ graffinity/tests/time_performance.py
@@ -15,18 +15,64 @@ class TestFoo(TestCase):
         gender_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
         age_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
         languages_func = lambda x: 5*(len(x) - len(set(x)))
+        friends_func = lambda x: 2.5*(len(x) - len(set(x)))
+        knowngames_func = lambda x: 2.5*(len(x) - len(set(x)))/len(x)
+        toplaywishlist_func = lambda x: 10*(len(x) - len(set(x)))/len(x)
+        favouritegames_func = lambda x: 20*(len(x) - len(set(x)))/len(x)
+        preferences_func = lambda x: 1*(len(x) - len(set(x)))
+        vetoes_func = lambda x: 1*(len(x) - len(set(x)))
+        gametype_func = lambda x: 15*(len(x) - len(set(x)))/len(x)
+        skill01_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill02_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill03_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill04_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill05_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill06_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill07_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill08_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill09_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        skill10_func = lambda x: abs(statistics.mean(x) - statistics.stdev(x))/statistics.mean(x)
+        guilds_func = lambda x: 5*(len(x) - len(set(x)))/len(x)
 
         funcs = {
             "gender": gender_func,
             "age": age_func,
             "languages": languages_func,
+            "friends" : friends_func,
+            "knowngames" : knowngames_func,
+            "toplaywishlist": toplaywishlist_func,
+            "favouritegames": favouritegames_func,
+            "preferences": preferences_func,
+            "vetoes": vetoes_func,
+            "gametype": gametype_func,
+            "skill01": skill01_func,
+            "skill02": skill02_func,
+            "skill03": skill03_func,
+            "skill04": skill04_func,
+            "skill05": skill05_func,
+            "skill06": skill06_func,
+            "skill07": skill07_func,
+            "skill08": skill08_func,
+            "skill09": skill09_func,
+            "skill10": skill10_func,
+            "guilds": guilds_func,
+
+
         }
 
-        affinityfunc = "gender_func(x) + 3.5*age_func(x) + 0.1*languages_func(x)"
+        affinityfunc = """gender_func(x) + age_func(x) + languages_func(x) + \
+        friends_func(x) + knowngames_func(x) + toplaywishlist_func(x) + \
+        favouritegames_func(x) + preferences_func(x) + vetoes_func(x) + \
+        gametype_func(x) + skill01_func(x) + skill02_func(x) + skill03_func(x) + \
+        skill04_func(x) + skill05_func(x) + skill06_func(x) + skill07_func(x) + \
+        skill08_func(x) + skill09_func(x) + skill10_func(x) + guilds_func(x)"""
 
         g = graffinity.Graffinity(data, funcs, affinityfunc)
-        g.calculate()
+        results = g.calculate()
+        
+        print(results['n1']['n18'],results['n18']['n1'])
 
 if __name__=="__main__":
-    import cProfile
-    cProfile.run('unittest.main()')
+#    import cProfile
+#    cProfile.run('unittest.main()')
+    unittest.main()
